2910c467542887f688d9a19c1c306a9ed76a53e3
domikul <kulczykdominikaa@gmail.com>
2020-09-03 17:45:42 +0200

    spring initalizr

---
+++ src/main/java/pl/polsl/Covid19TrackerServer/Covid19TrackerServerApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.Covid19TrackerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19TrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19TrackerServerApplication.class, args);
+	}
+
+}
